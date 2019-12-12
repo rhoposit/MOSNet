deb7581f5e6221394bd7dbc8d8175f98fb5ff9c0
rhoposit <j.williams@ed.ac.uk>
2019-12-12 12:17:13 +0000

    fixes CNN back to what it was before my debugging

--- model_keras.py
+++ model_keras.py
@@ -25,25 +25,25 @@ class CNN(object):
         re_input = Reshape((-1, 257, 1), input_shape=(-1, 257))(_input)
         
         # CNN
-        conv1 = (Conv2D(8, (3,3), strides=(1, 1), activation='relu', padding='same'))(re_input)
-        conv1 = (Conv2D(8, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv1)
-        conv1 = (Conv2D(8, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv1)
+        conv1 = (Conv2D(16, (3,3), strides=(1, 1), activation='relu', padding='same'))(re_input)
+        conv1 = (Conv2D(16, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv1)
+        conv1 = (Conv2D(16, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv1)
         
-        conv2 = (Conv2D(16, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv1)
-        conv2 = (Conv2D(16, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv2)
-        conv2 = (Conv2D(16, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv2)
+        conv2 = (Conv2D(32, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv1)
+        conv2 = (Conv2D(32, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv2)
+        conv2 = (Conv2D(32, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv2)
         
-        conv3 = (Conv2D(32, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv2)
-        conv3 = (Conv2D(32, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv3)
-        conv3 = (Conv2D(32, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv3)
+        conv3 = (Conv2D(64, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv2)
+        conv3 = (Conv2D(64, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv3)
+        conv3 = (Conv2D(64, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv3)
         
-        conv4 = (Conv2D(64, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv3)
-        conv4 = (Conv2D(64, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv4)
-        conv4 = (Conv2D(64, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv4)
+        conv4 = (Conv2D(128, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv3)
+        conv4 = (Conv2D(128, (3,3), strides=(1, 1), activation='relu', padding='same'))(conv4)
+        conv4 = (Conv2D(128, (3,3), strides=(1, 3), activation='relu', padding='same'))(conv4)
         
         # DNN
         flatten = TimeDistributed(Flatten())(conv4)
-        dense1=TimeDistributed(Dense(32, activation='relu'))(flatten)
+        dense1=TimeDistributed(Dense(64, activation='relu'))(flatten)
         dense1=Dropout(0.3)(dense1)
 
         frame_score=TimeDistributed(Dense(1), name='frame')(dense1)
