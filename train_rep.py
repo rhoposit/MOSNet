0204a4790b5d490feb1d911ac55553b5c6fc36ea
rhoposit <j.williams@ed.ac.uk>
2020-01-08 21:19:41 +0000

    debug

--- train_rep.py
+++ train_rep.py
@@ -118,7 +118,7 @@ else:
 model = MOSNet.build()
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(1e-4),metrics=['mean_absolute_error']
+    optimizer=tf.keras.optimizers.Adam(1e-4),metrics=['mean_absolute_error'],
     loss="mse")
     
 CALLBACKS = [
