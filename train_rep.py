653391cb76ab1deeeb1c27396ca9930a863a313b
rhoposit <j.williams@ed.ac.uk>
2020-01-10 22:44:08 +0000

    debug

--- train_rep.py
+++ train_rep.py
@@ -202,7 +202,7 @@ for i in tqdm(range(len(test_list))):
     _DS = utils.read_rep(os.path.join(BIN_DIR,filename+'.npy'))
         
     _DS = np.expand_dims(_DS, axis=3)
-    _DS = np.expand_dims(_DS, axis=0)
+    _DS = np.expand_dims(_DS, axis=1)
     print(_DS.shape)
     Average_score=model.predict(_DS, verbose=0, batch_size=1)
     MOS_Predict[i]=Average_score
