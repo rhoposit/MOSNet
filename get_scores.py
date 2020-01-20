ad564c0d49f5bf44bd258633eeaf7425aa259ea7
rhoposit <j.williams@ed.ac.uk>
2020-01-20 13:30:03 +0000

    update

--- get_scores.py
+++ get_scores.py
@@ -273,8 +273,8 @@ def get_scores(OUTPUT_DIR, data, resultsfile, reg_class_flag, logname):
 #        print(speakerID)
         print(spk_resultT.get_group(speakerID))
         print(spk_resultP.get_group(speakerID))
-        spk_true = spk_resultT.get_group(speakerID)
-        spk_predicted = spk_resultP.get_group(speakerID)
+        spk_true = spk_resultT.get_group(speakerID).apply(list)
+        spk_predicted = spk_resultP.get_group(speakerID).apply(list)
         if reg_class_flag == "R":
             print(spk_true)
             print(spk_predicted)
