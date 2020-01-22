6e2a033d7fc9b79f05c2c3253dbd17ff1bb5204c
rhoposit <j.williams@ed.ac.uk>
2020-01-22 15:43:23 +0000

    update

--- get_scores.py
+++ get_scores.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import argparse
 import tensorflow as tf
 from tensorflow import keras
-import model_rep
+#import model_rep
 import model
 import utils
 import random
