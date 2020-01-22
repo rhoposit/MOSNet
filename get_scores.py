232e6895733ae8c72868f9e23569c8c29425b1ba
rhoposit <j.williams@ed.ac.uk>
2020-01-22 17:05:11 +0000

    update

--- get_scores.py
+++ get_scores.py
@@ -276,7 +276,7 @@ for folder in F:
         logname = "log."+folder[12:-1]
         results_file = folder+"/res_df.pkl"
         items = folder.split("/")[2].split("_")
-        if len(items) <  == 11:
+        if len(items) <  11:
             continue
         print(items)
         data = items[3]
