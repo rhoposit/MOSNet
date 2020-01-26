b0f9c4e612055843984746f575260597e0f28245
rhoposit <j.williams@ed.ac.uk>
2020-01-26 15:04:47 +0000

    udpate

--- get_scores.py
+++ get_scores.py
@@ -49,18 +49,18 @@ def get_test_results(BIN_DIR, data, test_list, modelfile, resultsfile):
             speakerid = filepath[0]
             mos=float(filepath[3])
 
-        _feat = utils.read(os.path.join(BIN_DIR,filename+'.h5'))
-        _mag = _feat['mag_sgram']    
-        [Average_score, Frame_score]=model.predict(_mag, verbose=0, batch_size=1)
-        MOS_Predict[i]=Average_score
-        MOS_true[i]   =mos
+#        _feat = utils.read(os.path.join(BIN_DIR,filename+'.h5'))
+#        _mag = _feat['mag_sgram']    
+#        [Average_score, Frame_score]=model.predict(_mag, verbose=0, batch_size=1)
+#        MOS_Predict[i]=Average_score
+#        MOS_true[i]   =mos
 
 
-#        _DS = utils.read_rep(os.path.join(BIN_DIR,filename+'.npy'))
-#        _DS = np.expand_dims(_DS, axis=3)
-#        Average_score=model.predict(_DS, verbose=0, batch_size=1)
-#        MOS_Predict[i]=Average_score[0][0]
-#        MOS_true[i] =mos
+        _DS = utils.read_rep(os.path.join(BIN_DIR,filename+'.npy'))
+        _DS = np.expand_dims(_DS, axis=3)
+        Average_score=model.predict(_DS, verbose=0, batch_size=1)
+        MOS_Predict[i]=Average_score[0][0]
+        MOS_true[i] =mos
             
         df = df.append({'audio': filepath[0], 
                         'true_mos': MOS_true[i], 
@@ -305,8 +305,8 @@ sys.exit()
 '''
 
 # move my orig folders somewhere else
-F = glob.glob("./results_O2/output*/")
-#F = glob.glob("./results_O/output*DS-image*/")
+#F = glob.glob("./results_O2/output*/")
+F = glob.glob("./results_O/output*CNN*/")
 #F = ['./results_R/output_CNN_1_LA_orig/']
 print(F)
 MASTER_SORT_DICT = defaultdict(list)
@@ -316,14 +316,14 @@ MASTER_SORT_DICT = defaultdict(list)
 
 for folder in F:
 #    try:
-        logname = "logs_orig/"+"log."+folder[13:-1]
+        logname = "logs_CNN/"+"log."+folder[13:-1]
         results_file = folder+"/results.pkl"
         items = folder.split("/")[2].split("_")
         testfile = "data_LA/test_list.txt"
-        feats = "orig"
-#        feats = items[4]
-#        if feats == "xvec":
-#            feats = feats + "_" + items[5]
+#        feats = "orig"
+        feats = items[4]
+        if feats == "xvec":
+            feats = feats + "_" + items[5]
         input = open(testfile, "r")
         testlist = input.read().split("\n")[:-1]
         input.close()
